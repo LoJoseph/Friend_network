1bfc93a4c26e126c6badc45f96e8e2f3d20d68a6
LoJoseph <lovincy.joseph@gmail.com>
2016-12-20 23:27:15 +0100

    sign in page

--- signup.php
+++ signup.php
@@ -1,12 +1,29 @@
 <?php
-$DSN = 'mysql:host=localhost;dbname=network' 
+$DSN = 'mysql:host=localhost;dbname=network';
 $user= 'root';
 $mdp = '';
 $options = [
-	PDO::ATTR_ERRMODE => PDO::ERMMODE_WARNING,
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
 	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 ];
 
 $fnetwork = new PDO($DSN,$user,$mdp,$options);
-?
+?>
 
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Connexion</title>
+	</head>
+
+	<body>
+		<main>
+			<form action="connexion.php" method="POST">
+				<label>Pseudo</label><input type="text" name="pseudo">
+				<label>Mot de passe</label><input type="text" name="pseudo">
+			</form>
+		</main>
+		
+	</body>
+</html>
