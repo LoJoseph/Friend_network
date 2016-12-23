e0dbc0bca693425fd1991aa2ef7ab69e5618c817
LoJoseph <lovincy.joseph@gmail.com>
2016-12-23 15:28:56 +0100

    forms update

---
+++ inc/connexion_bd.inc.php
@@ -0,0 +1,11 @@
+<?php
+$DSN = 'mysql:host=localhost;dbname=network';
+$user= 'root';
+$mdp = '';
+$options = [
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+];
+
+$fnetwork = new PDO($DSN,$user,$mdp,$options);
+?>
--- signup.php
+++ signup.php
@@ -1,14 +1,6 @@
-<?php
-$DSN = 'mysql:host=localhost;dbname=network';
-$user= 'root';
-$mdp = '';
-$options = [
-	PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
-	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-];
-
-$fnetwork = new PDO($DSN,$user,$mdp,$options);
+<? require_once 'inc/connexion_bd.inc.php';
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -18,12 +10,50 @@ $fnetwork = new PDO($DSN,$user,$mdp,$options);
 	</head>
 
 	<body>
+		<header>
+	
+		</header>
+
 		<main>
-			<form action="connexion.php" method="POST">
-				<label>Pseudo</label><input type="text" name="pseudo">
-				<label>Mot de passe</label><input type="text" name="pseudo">
-			</form>
+			<!-- partie connexion -->
+			<section>
+				<form action="signup.php" method="POST">
+
+					<label for="pseudo">Pseudo</label>
+					<input type="text" name="pseudo" id="pseudo">
+
+					<label for="mdp">Mot de passe</label>
+					<input type="text" name="mdp_to_confirm" id="mdp" >
+
+					<input type="submit" name="confirm" value="Inscription">
+				</form>
+			</section>
+
+			<!-- Partie formulaire d'inscription -->
+			<section>
+				<form action="signup.php" method= "POST">
+
+					<label for="prenom">Prénom</label>
+					<input type="text" name="firstname" id="prenom">
+
+					<label for="nom">nom</label>
+					<input type="text" name="name" id="nom">
+
+					<label for="homme">Homme</label>
+					<input type="radio" value="homme" name="gender">
+
+					<label for="femme">Femme</label>
+					<input type="radio" value="femme" name="gender" id="femme">
+
+					<label for="age">Age</label>
+					<input type="integer" name="age" id="age">
+
+					<label for="new_pseudo">Choisissez un pseudo</label>
+					<input type="text" name="ins_pseudo" id="new_pseudo">
+
+					<input type="submit" name="confirm_inscription" value="Inscription">
+				</form>
+			</section>
 		</main>
-		
 	</body>
 </html>
